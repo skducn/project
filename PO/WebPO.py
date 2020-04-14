23cd7eef0067ce0b04b6f023c3e4f0885c8af2d8
skducn <skducn@163.com>
2020-04-14 11:35:11 +0800

    fromMac

--- PO/WebPO.py
+++ PO/WebPO.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # *******************************************************************************************************************************
-# Author     : John123
+# Author     : John
 # Date       : 2018-7-2
 # Description: webdriverPO 对象层
 # geckodriver 0.14.0 for selenium3.0 下载地址：https://github.com/mozilla/geckodriver/releases
