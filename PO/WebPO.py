429fe14272ecf0d7eac070af73c397d7ea717dcf
skducn <skducn@163.com>
2020-04-14 11:26:24 +0800

    'test'

--- PO/WebPO.py
+++ PO/WebPO.py
@@ -10,7 +10,6 @@
 # Q1：WebDriverException:Message:'geckodriver'executable needs to be in Path
 # A1：geckodriver是原生态的第三方浏览器，对于selenium3.x版本使用geckodriver来驱动firefox，需下载geckodriver.exe,下载地址：https://github.com/mozilla/geckodriver/releases
 # 将 geckodriver 放在 C:\Python38\Scripts
-
 # Q2：MAC 移动chromedriver时报错，如 sudo mv chromedriver /usr/bin 提示： Operation not permitted
 # A2: 重启按住command + R,进入恢复模式，实用工具 - 终端，输入 csrutil disable , 重启电脑。
 # *******************************************************************************************************************************
