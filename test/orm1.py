fe30b2b87b9be7f8fd78d58e2031439074e1acbc
skducn <skducn@163.com>
2020-04-28 18:01:41 +0800

    'fromCompany'

--- test/orm1.py
+++ test/orm1.py
@@ -6,19 +6,24 @@
 # https://blog.csdn.net/GVFDBDF/article/details/49254701?depth_1-utm_source=distribute.pc_relevant.none-task-blog-BlogCommendFromBaidu-25&utm_source=distribute.pc_relevant.none-task-blog-BlogCommendFromBaidu-25
 #***************************************************************
 
-__auth__ = 'peic'
-
-'''
+'''''
 选择MySQL作为网站的后台数据库
+
 执行SQL语句进行操作，并将常用的SELECT、INSERT等语句进行函数封装
+
 在异步框架的基础上，采用aiomysql作为数据库的异步IO驱动
+
 将数据库中表的操作，映射成一个类的操作，也就是数据库表的一行映射成一个对象(ORM)
+
 整个ORM也是异步操作
-预备知识：Python协程和异步IO(yield from的使用)、SQL数据库操作、元类、面向对象知识、Python语法
+
+预备知识：Python协程和异步IO(await的使用)、SQL数据库操作、元类、面向对象知识、Python语法
+
 # -*- -----  思路  ----- -*-
     如何定义一个user类，这个类和数据库中的表User构成映射关系，二者应该关联起来，user可以操作表User
 
     通过Field类将user类的属性映射到User表的列中，其中每一列的字段又有自己的一些属性，包括数据类型，列名，主键和默认值
+
 '''
 
 import asyncio, logging
@@ -31,47 +36,34 @@ import aiomysql
 def log(sql, args=()):
     logging.info('SQL: %s' % (sql))
 
+    # Mysql_PO = MysqlPO("192.168.0.195", "root", "Zy123456", "bitest", 3306)  # BI 开发数据库
+
 
 # 创建一个全局的连接池，每个HTTP请求都从池中获得数据库连接
-@asyncio.coroutine
-def create_pool(loop, **kw):
+
+async def create_pool(loop, **kw):
     logging.info('create database connection pool...')
 
     # 全局变量__pool用于存储整个连接池
     global __pool
-    __pool = yield from aiomysql.create_pool(
-        # **kw参数可以包含所有连接需要用到的关键字参数
-        # 默认本机IP
-        host=kw.get('host', 'localhost'),
-        user=kw['user'],
-        password=kw['password'],
-        db=kw['db'],
-        port=kw.get('port', 3306),
-        charset=kw.get('charset', 'utf8'),
-        autocommit=kw.get('autocommit', True),
-        # 默认最大连接数为10
-        maxsize=kw.get('maxsize', 10),
-        minsize=kw.get('minisize', 1),
-
-        # 接收一个event_loop实例
-        loop=loop
-    )
+    # **kw参数可以包含所有连接需要用到的关键字参数
+    __pool = await aiomysql.create_pool(host=kw.get('host', '192.168.0.195'),user=kw['root'],password=kw['Zy123456'],db=kw['bitest'],port=kw.get('port', 3306),charset=kw.get('charset', 'utf8'),autocommit=kw.get('autocommit', True),minsize=kw.get('minisize', 1),loop=loop)
 
 
 # 封装SQL SELECT语句为select函数
-def select(sql, args, size=None):
+async def select(sql, args, size=None):
     log(sql, args)
     global __pool
 
-    # -*- yield from 将会调用一个子协程，并直接返回调用的结果
-    # yield from从连接池中返回一个连接
-    with (yield from __pool) as conn:
+    # -*- await 将会调用一个子协程，并直接返回调用的结果
+    # await从连接池中返回一个连接
+    with (await __pool) as conn:
         # DictCursor is a cursor which returns results as a dictionary
-        cur = yield from conn.cursor(aiomysql.DictCursor)
+        cur = await conn.cursor(aiomysql.DictCursor)
 
         # 执行SQL语句
         # SQL语句的占位符为?，MySQL的占位符为%s
-        yield from cur.execute(sql.replace('?', '%s'), args or ())
+        await cur.execute(sql.replace('?', '%s'), args or ())
 
         # 根据指定返回的size，返回查询的结果
         if size:
@@ -81,37 +73,39 @@ def select(sql, args, size=None):
             # 返回所有查询结果
             rs = fetchall()
 
-        yield from cur.close()
+        await cur.close()
         logging.info('rows return: %s' % (len(rs)))
         return rs
 
+    # 封装INSERT, UPDATE, DELETE
 
-# 封装INSERT, UPDATE, DELETE
+
 # 语句操作参数一样，所以定义一个通用的执行函数
 # 返回操作影响的行号
-@asyncio.coroutine
+
 def execute(sql, args):
     log(sql, args)
     global __pool
-    with (yield from __pool) as conn:
+    with ( __pool) as conn:
         try:
             # execute类型的SQL操作返回的结果只有行号，所以不需要用DictCursor
-            cur = yield from conn.cursor()
+            cur =  conn.cursor()
             cur.execute(sql.replace('?', '%s'), args)
             affectedLine = cur.rowcount
-            yield from cur.close()
+            cur.close()
         except BaseException as e:
             raise
         return affectedLine
 
+    # 根据输入的参数生成占位符列表
 
-# 根据输入的参数生成占位符列表
+
 def create_args_string(num):
     L = []
     for n in range(num):
         L.append('?')
 
-    # 以','为分隔符，将列表合成字符串
+        # 以','为分隔符，将列表合成字符串
     return (','.join(L))
 
 
@@ -124,12 +118,14 @@ class Field(object):
         self.primary_key = primary_key
         self.default = default
 
-    # 当打印(数据库)表时，输出(数据库)表的信息:类名，字段类型和名字
+        # 当打印(数据库)表时，输出(数据库)表的信息:类名，字段类型和名字
+
     def __str__(self):
         return ('<%s, %s: %s>' % (self.__class__.__name__, self.column_type, self.name))
 
+    # -*- 定义不同类型的衍生Field -*-
 
-# -*- 定义不同类型的衍生Field -*-
+
 # -*- 表的不同列的字段的类型不一样
 
 class StringField(Field):
@@ -156,8 +152,8 @@ class TextField(Field):
     def __init__(self, name=None, default=None):
         super().__init__(name, 'Text', False, default)
 
+    # -*-定义Model的元类
 
-# -*-定义Model的元类
 
 # 所有的元类都继承自type
 # ModelMetaclass元类定义了所有Model基类(继承ModelMetaclass)的子类实现的操作
@@ -182,7 +178,7 @@ class ModelMetaclass(type):
         if name == 'Model':
             return type.__new__(cls, name, bases, attrs)
 
-        # 获取table名词
+            # 获取table名词
         tableName = attrs.get('__table__', None) or name
         logging.info('found model: %s (table: %s)' % (name, tableName))
 
@@ -203,20 +199,20 @@ class ModelMetaclass(type):
                     # 如果此时类实例的以存在主键，说明主键重复了
                     if primaryKey:
                         raise StandardError('Duplicate primary key for field: %s' % k)
-                    # 否则将此列设为列表的主键
+                        # 否则将此列设为列表的主键
                     primaryKey = k
                 else:
                     fields.append(k)
-        # end for
+                    # end for
 
         if not primaryKey:
             raise StandardError('Primary key is nor founnd')
 
-        # 从类属性中删除Field属性
+            # 从类属性中删除Field属性
         for k in mappings.keys():
             attrs.pop(k)
 
-        # 保存除主键外的属性名为``（运算出字符串）列表形式
+            # 保存除主键外的属性名为``（运算出字符串）列表形式
         escaped_fields = list(map(lambda f: '`%s`' % f, fields))
 
         # 保存属性和列的映射关系
@@ -239,8 +235,9 @@ class ModelMetaclass(type):
 
         return type.__new__(cls, name, bases, attrs)
 
+    # 定义ORM所有映射的基类：Model
 
-# 定义ORM所有映射的基类：Model
+
 # Model类的任意子类可以映射一个数据库表
 # Model类可以看作是对所有数据库表操作的基本定义的映射
 
@@ -278,9 +275,8 @@ class Model(dict, metaclass=ModelMetaclass):
 
     @classmethod
     # 类方法有类变量cls传入，从而可以用cls做一些相关的处理。并且有子类继承时，调用该类方法时，传入的类变量cls是子类，而非父类。
-    @asyncio.coroutine
-    def findAll(cls, where=None, args=None, **kw):
-        '''find objects by where clause'''
+    async def findAll(cls, where=None, args=None, **kw):
+        '''''find objects by where clause'''
         sql = [cls.__select__]
 
         if where:
@@ -306,66 +302,61 @@ class Model(dict, metaclass=ModelMetaclass):
                 args.extend(limit)
             else:
                 raise ValueError('Invalid limit value: %s' % str(limit))
-        rs = yield from select(' '.join(sql), args)
+        rs = await select(' '.join(sql), args)
         return [cls(**r) for r in rs]
 
     @classmethod
-    @asyncio.coroutine
-    def findNumber(cls, selectField, where=None, args=None):
-        '''find number by select and where.'''
+    async def findNumber(cls, selectField, where=None, args=None):
+        '''''find number by select and where.'''
         sql = ['select %s __num__ from `%s`' % (selectField, cls.__table__)]
         if where:
             sql.append('where')
             sql.append(where)
-        rs = yield from select(' '.join(sql), args, 1)
+        rs = await select(' '.join(sql), args, 1)
         if len(rs) == 0:
             return None
         return rs[0]['__num__']
 
     @classmethod
-    @asyncio.coroutine
-    def find(cls, primarykey):
-        '''find object by primary key'''
-        rs = yield from select('%s where `%s`=?' % (cls.__select__, cls__primary_key__), [primarykey], 1)
+    async def find(cls, primarykey):
+        '''''find object by primary key'''
+        rs = await select('%s where `%s`=?' % (cls.__select__, cls__primary_key__), [primarykey], 1)
         if len(rs) == 0:
             return None
         return cls(**rs[0])
 
-    @asyncio.coroutine
-    def save(self):
+    async def save(self):
         args = list(map(self.getValueOrDefault, self.__fields__))
         args.append(self.getValueOrDefault(self.__primary_key__))
-        rows = yield from execute(self.__insert__, args)
+        rows = await execute(self.__insert__, args)
         if rows != 1:
             logging.warn('failed to insert record: affected rows: %s' % rows)
 
-    @asyncio.coroutine
-    def update(self):
+    async def update(self):
         args = list(map(self.getValue, self.__fields__))
         args.append(self.getValue(self.__primary_key__))
-        rows = yield from execute(self.__updata__, args)
+        rows = await execute(self.__updata__, args)
         if rows != 1:
             logging.warn('failed to update by primary key: affected rows: %s' % rows)
 
-    @asyncio.coroutine
-    def remove(self):
+    async def remove(self):
         args = [self.getValue(self.__primary_key__)]
-        rows = yield from execute(self.__updata__, args)
+        rows = await execute(self.__updata__, args)
         if rows != 1:
             logging.warn('failed to remove by primary key: affected rows: %s' % rows)
 
 
 if __name__ == '__main__':
-    class User(Model):
+    class Bi_anaesthesia_dept(Model):
         # 定义类的属性到列的映射：
         id = IntegerField('id', primary_key=True)
-        name = StringField('username')
-        email = StringField('email')
-        password = StringField('password')
 
-    # 创建一个实例：
-    u = User(id=12345, name='peic', email='peic@python.org', password='password')
+
+        # 创建一个实例：
+
+
+    u = Bi_anaesthesia_dept(id=12345)
     print(u)
     # 保存到数据库：
     u.save()
-    print(u)
+    print(u)  
