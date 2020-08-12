df9167689c5b6a28b38524a40ec00423074276c1
skducn <skducn@163.com>
2020-08-12 18:21:48 +0800

    DESKTOP-EOCO1V0_20200812_182148

--- projectPush.bat
+++ projectPush.bat
@@ -1,4 +1,13 @@
+@echo off
+
 git add .
-git commit -m %date:~0,4%%date:~5,2%%date:~8,2%_%time:~0,2%%time:~3,2%%time:~6,2%
-git push origin HEAD:master
+
+
+
+git commit -m %computername%_%date:~0,4%%date:~5,2%%date:~8,2%_%time:~0,2%%time:~3,2%%time:~6,2%
+
+
+
+git push origin master
+
 
