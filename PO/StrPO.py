9c1541b280fe243bbe862c37c020a5c790a049f4
skducn <skducn@163.com>
2020-11-12 16:30:22 +0800

    DESKTOP-EOCO1V0_20201112_6:3021

--- PO/StrPO.py
+++ PO/StrPO.py
@@ -6,13 +6,25 @@
 # *********************************************************************
 
 import sys
+
+'''
+1.1 字符串转列表
+1.2 字符串转元组
+1.3 字符串转字典
+
+2 判断字符串是否为数字
+3 判断字符串中是否包含中文
+4 判断字符串是否全部是中文
+
+'''
+
+
 class StrPO():
 
     def __init__(self):
         pass
 
-
-    # 1.1，字符串转列表
+    # 1.1 字符串转列表
     def str2list(self, varStr=None, varMode='digit'):
         # print(Str_PO.str2list("1,2,3"))  # [1, 2, 3]    //列表元素是数字， 默认字符串是数字，转换后仍然是数字作为列表元素。
         # print(Str_PO.str2list("a1,2,3"))  # ['a1', '2', '3']
@@ -35,14 +47,14 @@ class StrPO():
             except:
                 print("[ERROR], " +  sys._getframe(1).f_code.co_name + ", line " + str(sys._getframe(1).f_lineno) + ", in " + sys._getframe(0).f_code.co_name + ", SourceFile '" + sys._getframe().f_code.co_filename + "'")
 
-    # 1.2，字符串转元组
+    # 1.2 字符串转元组
     def str2tuple(self, varStr):
         try:
             return tuple(eval(varStr))
         except:
             print("[ERROR], " +  sys._getframe(1).f_code.co_name + ", line " + str(sys._getframe(1).f_lineno) + ", in " + sys._getframe(0).f_code.co_name + ", SourceFile '" + sys._getframe().f_code.co_filename + "'")
 
-    # 1.3，字符串转字典
+    # 1.3 字符串转字典
     def str2dict(self, varStr):
         # 技巧，如果输出结果中是单引号，这一组就是字典,如：{'a': '123', 'b': 456}
         # 技巧，如果输出结果中是双引号，这一组就是字符串，如：{"a": "192.168.1.1", "b": "192.168.1.2"}
@@ -52,7 +64,7 @@ class StrPO():
             print("[ERROR], " +  sys._getframe(1).f_code.co_name + ", line " + str(sys._getframe(1).f_lineno) + ", in " + sys._getframe(0).f_code.co_name + ", SourceFile '" + sys._getframe().f_code.co_filename + "'")
 
 
-    # 2，判断字符串是否为数字
+    # 2 判断字符串是否为数字
     def isNumberByStr(self, s):
         # 可识别中文，阿拉伯语，泰语等数字
         # print(Str_PO.isNumberByStr('foo'))  # False
@@ -69,18 +81,16 @@ class StrPO():
             return True
         except ValueError:
             pass
-
         try:
             import unicodedata
             unicodedata.numeric(s)
             return True
         except (TypeError, ValueError):
             pass
-
         return False
 
 
-    # 3，判断字符串中是否包含中文
+    # 3 判断字符串中是否包含中文
     def isContainChinese(self, varStr):
         for ch in varStr:
             if u'\u4e00' <= ch <= u'\u9fa5':
@@ -88,7 +98,7 @@ class StrPO():
         return False
 
 
-    # 4，判断字符串是否全部是中文
+    # 4 判断字符串是否全部是中文
     def isChinese(self, varStr):
         for _char in varStr:
             if not '\u4e00' <= _char <= '\u9fa5':
