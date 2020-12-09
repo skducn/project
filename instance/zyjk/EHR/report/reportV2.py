cf321dd9ea5f6ef685aee936301014e8aee03d0b
skducn <skducn@163.com>
2020-12-09 12:36:47 +0800

    DESKTOP-EOCO1V0_20201209_2:3646

--- instance/zyjk/EHR/report/reportV2.py
+++ instance/zyjk/EHR/report/reportV2.py
@@ -5,45 +5,24 @@
 # Description: 健康档案质控报告V2
 # *****************************************************************
 
-from PO.TimePO import *
-time_PO = TimePO()
-from PO.ListPO import *
-List_PO = ListPO()
-from PO.ExcelPO import *
-from PO.BasePO import *
-Base_PO = BasePO(object)
+import sys
+sys.path.append("../../../../")
+from instance.zyjk.EHR.PageObject.ReportPO import *
+Report_PO = ReportPO()
 
 
-Excel_PO = ExcelPO("v2.xlsx")
+# print("表1 = 1.常住人口电子健康档案工作指标 >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+# Report_PO.colResult(["表1.电子健康档案建档率", "=", "辖区内常住人口建立电子健康档案人数", "/", "辖区内常住人口数", "*100"], "保留2位")
+# Report_PO.colResult(["表1.目标签约人数", "=", "辖区内常住人口数", "*0.3"], "取整")
+# Report_PO.colResult(["表1.签约完成率", "=", "实际签约人数", "/", "目标签约人数", "*100"], "保留2位")
 
-print("表1 = 1.常住人口电子健康档案工作指标 >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" )
-tbl1 = Excel_PO.l_getRowData("表1")
-tbl1.pop(0)
-print("[表1_c1_电子健康档案建档率]")
-for i in range(len(tbl1)):
-    if round((tbl1[i][3])/(tbl1[i][2]) * 100, 2) == round(tbl1[i][4], 2):
-        # print("[ok] " + str(i+2))
-        Excel_PO.setCellColor(i + 2, 5, "00E400", "表1")
-    else:
-        print("[error] No." + str(i+2) + "，表格值" + str(round(tbl1[i][4], 2)) + "，测试值" + str(round(tbl1[i][3] / tbl1[i][2] * 100, 2)))
-        Excel_PO.setCellColor(i + 2, 5, "FF0000", "表1")
 
-print("\n[表1_c2_目标签约人数]")
-for i in range(len(tbl1)):
-    if (int(tbl1[i][2] * 0.3)) == tbl1[i][5]:
-        # print("[ok] " + str(i+2))
-        Excel_PO.setCellColor(i + 2, 6, "00E400", "表1")
-    else:
-        print("[error] No." + str(i + 2) + "，表格值" + str(round(tbl1[i][5], 2)) + "，测试值" + str(round(tbl1[i][2] * 0.3, 2)))
-        Excel_PO.setCellColor(i + 2, 6, "FF0000", "表1")
+print("\n表5 = 5.签约居民中65岁及以上老年人电子健康档案工作指标 >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+Report_PO.colResult(["表5.签约居民中65岁及以上老年人占家庭医生签约总人数占比", "=", "签约居民中65岁及以上老年人", "/", "全部签约人数", "*100"], "保留2位")
+Report_PO.colResult(["表5.签约居民中65岁及以上老年人电子健康档案建档率", "=", "签约居民中65岁及以上老年人有电子健康档案的人数", "/", "全部签约人数", "*100"], "保留2位")
 
-print("\n[表1_c3_签约完成率]")
-for i in range(len(tbl1)):
-    if round(tbl1[i][6] / tbl1[i][5] * 100, 2) == round(tbl1[i][7], 2):
-        # print("[ok] " + str(i+2))
-        Excel_PO.setCellColor(i + 2, 8, "00E400", "表1")
-    else:
-        print("[error] No." + str(i + 2) + "，表格值" + str(round(tbl1[i][7], 2)) + "，测试值" + str(round(tbl1[i][6] / tbl1[i][5] * 100, 2)))
-        Excel_PO.setCellColor(i + 2, 8, "FF0000", "表1")
 
-Excel_PO.save()
+print("\n表6 = 6.签约居民中高血压患者电子健康档案工作指标 >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+Report_PO.colResult(["表6.签约居民中高血压患者管理人数占家庭医生签约总人数占比", "=", "签约居民中高血压患者管理人数", "/", "签约人数", "*100"], "保留2位")
+Report_PO.colResult(["表6.签约居民中高血压患者电子健康档案建档率", "=", "签约居民中高血压患者有电子健康档案的人数", "/", "签约居民中高血压患者管理人数", "*100"], "保留2位")
+Report_PO.save()
