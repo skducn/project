eb2ac98b7adc432b29f5bf6bd3ad56c612430e64
jinhao <h.jin@zy-healthtech.com>
2021-01-29 11:23:52 +0800

    2020.4.1 add readme.md

--- script/class/Polymorphism.py
+++ script/class/Polymorphism.py
@@ -33,3 +33,46 @@ dog.run()  # 狗跑
 pig.run()  # 猪跑
 # 同属一个父类，但是他们在run这个方法上表现出不能的形态，这就是多态。
 # 多态性是指当不同的对象收到相同的消息时，产生不同的动作。
+
+
+#
+# # 使用多态，不同的子类对象可以共用一个函数
+# # 需求：饲养员喂养猫和老鼠，老虎等动物
+# # 分析：
+# # 定义饲养员的类和动物的类
+# # 定义老鼠类，猫类等继承动物类
+# # 在饲养员中定义类成员函数，喂养
+#
+# class Person(object):
+#     '''
+#     def feedCat(self,cat):
+#         print("喂养" + cat.name)
+#     def feedMouse(self,mouse):
+#         print("喂养" + mouse.name)
+#     def feedTiger(self,tiger):
+#         print("喂养" + tiger.name)
+#     '''
+#
+#     def feedAnimal(self, ani):
+#         print("喂养" + ani.name)
+#
+#
+# class Animal(object):
+#     def __init__(self, name):
+#         self.name = name
+#
+#     def eat(self):
+#         print("eating")
+#
+#
+# class Mouse(Animal):
+#     def __init__(self, name):
+#         super(Mouse, self).__init__(name)
+#
+#
+# # 1.创建一个饲养员的对象
+# p = Person()
+# # 2.创建Mouse的对象
+# m = Mouse("Tom")
+# # 3.饲养员执行自己的行为
+# p.feedAnimal(m)  # 喂养Tom
--- script/class/class2.py
+++ script/class/class2.py
@@ -7,38 +7,75 @@
 # 2，静态方法，使用@staticmethod，不能访问类变量
 # *******************************************************************************************************************************
 
-'''类方法，使用@classmethod'''
-class A:
+
+# todo: 类方法 @classmethod
+# 类方法是将类本身作为对象进行操作的方法，可以被继承
+print("类方法 @classmethod".center(100, "-"))
+class ClassWay:
     num = 1
     @classmethod
-    def func(cls):  # 类方法需要传递cls参数，类似于self
-        print('num', cls.num)  # 类方法可以访问类变量，如num
+    def func(cls):  # 类方法第一个参数为cls，自动绑定到类。
+        print('num=', cls.num)  # 类方法可以访问类变量，如num
 
-A.func()  # num 1
-a = A()
-a.func()  # num 1
-print(A.func)  # <bound method A.func of <class '__main__.A'>>
-print(a.func)  # <bound method A.func of <class '__main__.A'>>
-print("~~~~~~~~~~~~")
+ClassWay.func()  # num= 1
+a = ClassWay()
+a.func()  # num= 1
+print(ClassWay.func)  # <bound method ClassWay.func of <class '__main__.ClassWay'>>
+print(a.func)  # <bound method ClassWay.func of <class '__main__.ClassWay'>>
+print(type(a.func))  # <class 'method'>
+print(type(a))  # <class '__main__.ClassWay'>
+print(type(ClassWay))  # <class '__main__.ClassWay'>
 
-'''静态方法，使用@staticmethod'''
-class B:
+
+# todo: 静态方法 @staticmethod
+print("静态方法 @staticmethod".center(100, "-"))
+class StaticWay:
     num = 2  # 静态方法不能访问类变量
     @staticmethod
     def func():
         a = 33
         print(a)
 
-B.func() # 33
-b = B()
-c = B()
+StaticWay.func() # 33
+b = StaticWay()
+c = StaticWay()
 b.func()  # 33
 c.func()   # 33
-print("~~~~~~~~~~~~")
-
-'''由于使用了staticmathod，以下三个地址共享同一块数据, '''
-print (B.func) # <function B.func at 0x0000020478715040>
-print (b.func) # <function B.func at 0x0000020478715040>
-print (c.func) # <function B.func at 0x0000020478715040>
 
 
+# 由于使用了staticmathod，所以静态方法的地址共享同一块数据
+print(StaticWay.func) # <function B.func at 0x0000020478715040>
+print(b.func) # <function B.func at 0x0000020478715040>
+print(c.func) # <function B.func at 0x0000020478715040>
+
+
+
+
+
+class ClassTest(object):
+    __num = 0
+
+    @classmethod
+    def addNum(cls):
+        cls.__num += 1
+
+    @classmethod
+    def getNum(cls):
+        return cls.__num
+
+    # 这里我用到魔术方法__new__，主要是为了在创建实例的时候调用累加方法。
+    def __new__(self):
+        ClassTest.addNum()
+        print("执行1")
+        # return super(ClassTest, self).__new__(self)  # <__main__.Student object at 0x0000012456D16880>
+b
+
+class Student(ClassTest):
+    def __init__(self):
+        self.name = ''
+        print("执行2")
+
+a = Student()
+b = Student()
+c = Student()
+print(ClassTest.getNum())
