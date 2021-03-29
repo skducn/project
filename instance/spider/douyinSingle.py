555f5a1d0a65b08679c40b4d9a34528c5484b73d
skducn <skducn@163.com>
2021-03-29 09:07:26 +0800

    fromMac

--- instance/spider/douyinSingle.py
+++ instance/spider/douyinSingle.py
@@ -11,6 +11,7 @@ import requests, re, os, platform
 from time import sleep
 
 
+
 def downloadSingleVideo(url):
 	session = requests.session()
 	headers = {'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1'}
