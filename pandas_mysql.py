5fa633f569221be32d6ec055745d48fc27c2a917
jinhao <h.jin@zy-healthtech.com>
2021-04-12 18:01:27 +0800

    DESKTOP-DO7AEPU_20210412_8:0126

--- pandas_mysql.py
+++ pandas_mysql.py
@@ -2,19 +2,21 @@
 # *****************************************************************
 # Author     : John
 # Date       : 2021-4-8
-# Description: pandas 连接数据库 for mysql
+# Description: pandas 操作mysql数据库
 # pandas dataframe.to_sql() 用法  , https://www.jianshu.com/p/d615699ff254
 # *****************************************************************
 
 import pandas as pd
 from sqlalchemy import create_engine
 
-# 初始化数据库连接，使用pymysql模块
+# 使用pymysql模块初始化数据库连接
 engine = create_engine('mysql+pymysql://ceshi:123456@192.168.0.233:3336/TD_OA')
 
 # 读表
-df = pd.read_sql("user", engine)
-
+sql_cmd = "select * from user_jh where user_name='金浩'"
+df = pd.read_sql(sql=sql_cmd, con=engine)
+print(df.head())
+print(df["USER_NAME"][0])
 # 写表方法1，原表是数据库表，将DATAFRAME to_sql成新表。（会有以下几个问题）
 # 问题1：目标表的数据类型变为数字型和字符型。
 # 问题2：没有了primarykey
@@ -47,53 +49,57 @@ df = pd.read_sql("user", engine)
 #         `NR_OF_CHILDREN` BIGINT,
 #         CONSTRAINT emp_pk PRIMARY KEY (`EMP_ID`)
 # )
-
-# 写表方法3，原表是数据库表，将DATAFRAME to_sql成新表。（多了一个index字段，或通过index=False不创建索引）
-# Copy table structure
-with engine.connect() as con:
-    con.execute('DROP TABLE if exists user_jh')
-    con.execute('CREATE TABLE user_jh LIKE user;')  # 复制新表 user_jh
-
-# print(df.shape)
-# print(df.info())
-# print(df.columns)
-# print(df.columns.values)
-# print(df.columns.values[3])
-# df.columns = [x.strip() for x in df.columns.values]   # 将列表名中前后空格去掉了。
-
-print(df.duplicated().sum())
-
-print(df.describe().T)
-
-print(df[df.ONLINE < 5000])
-print(df[df.ONLINE < 5000].count()["UID"])
-
-print(df.isnull().sum())
-
-print(df[df.IMEI.isnull()].head())
-
-print(df.loc[df.IMEI.isnull(), "USER_NAME"])
-# df.USER_NAME = df.USER_NAME + "NULL"
-# str(df.USER_NAME).replace("NULL", "")
-
-x = df.loc[df.IMEI.isnull(), "USER_NAME"]
-df.loc[df.IMEI.isnull(), "USER_NAME"] = [i + "NULL" for i in x]
-
-
-# # # 将新建的DataFrame储存为MySQL中的数据表，储存index列
-df.to_sql('user_jh', engine, index=False, if_exists='append')
-
-# sql = ''' select * from user_test; '''
-# # read_sql_query的两个参数: sql语句， 数据库连接
-# df = pd.read_sql_query(sql, engine)
 #
-# # 输出表的查询结果
-# print(df['USER_NAME'])
+# # 写表方法3，原表是数据库表，将DATAFRAME to_sql成新表。（多了一个index字段，或通过index=False不创建索引）
+# # Copy table structure
+# with engine.connect() as con:
+#     con.execute('DROP TABLE if exists user_jh')
+#     con.execute('CREATE TABLE user_jh LIKE user;')  # 复制新表 user_jh
 #
-# # # 新建pandas中的DataFrame, 只有id,num两列
-# df = pd.DataFrame({'id': [1, 2, 3, 4], 'name': ['zhangsan', 'lisi', 'wangwu', 'zhuliu']})
-# print(df)
+# # print(df.shape)
+# # print(df.info())
+# # print(df.columns)
+# # print(df.columns.values)
+# # print(df.columns.values[3])
+# # df.columns = [x.strip() for x in df.columns.values]   # 将列表名中前后空格去掉了。
 #
-
-# df.to_sql('user_copy', engine, index=True) # mydf表名，engine：存到相应的数据库下面
-# # print('Read from and write to Mysql table successfully!')
+# print(df.duplicated().sum())
+#
+# print(df.describe().T)
+#
+# print(df[df.ONLINE < 5000])
+# print(df[df.ONLINE < 5000].count()["UID"])
+#
+# print(df.isnull().sum())
+#
+# print(df[df.IMEI.isnull()].head())
+#
+# print(df.loc[df.IMEI.isnull(), "USER_NAME"])
+# # df.USER_NAME = df.USER_NAME + "NULL"
+# # str(df.USER_NAME).replace("NULL", "")
+#
+# x = df.loc[df.IMEI.isnull(), "USER_NAME"]
+# df.loc[df.IMEI.isnull(), "USER_NAME"] = [i + "NULL" for i in x]
+#
+#
+# # # # 将新建的DataFrame储存为MySQL中的数据表，储存index列
+# df.to_sql('user_jh', engine, index=False, if_exists='append')
+#
+#
+#
+#
+#
+# # sql = ''' select * from user_test; '''
+# # # read_sql_query的两个参数: sql语句， 数据库连接
+# # df = pd.read_sql_query(sql, engine)
+# #
+# # # 输出表的查询结果
+# # print(df['USER_NAME'])
+# #
+# # # # 新建pandas中的DataFrame, 只有id,num两列
+# # df = pd.DataFrame({'id': [1, 2, 3, 4], 'name': ['zhangsan', 'lisi', 'wangwu', 'zhuliu']})
+# # print(df)
+# #
+#
+# # df.to_sql('user_copy', engine, index=True) # mydf表名，engine：存到相应的数据库下面
+# # # print('Read from and write to Mysql table successfully!')
