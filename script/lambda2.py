71281edb3560a7e622dba0e437a4662d06d0048f
jinhao <h.jin@zy-healthtech.com>
2021-05-28 09:00:26 +0800

    DESKTOP-DO7AEPU_20210528_9:0025

--- script/lambda2.py
+++ script/lambda2.py
@@ -3,7 +3,7 @@
 # Author     : John
 # Date       : 2017-12-19
 # Description: 匿名函数 lambda：是指一类无需定义标识符（函数名）的函数或子程序。
-# lambda 函数可以接收任意多个参数 (包括可选参数) 并且返回单个表达式的值。
+# lambda 函数可接收多个参数 (包括可选参数) 并且返回单个表达式的值。
 # 要点：
 # 1，lambda 函数不能包含命令；
 # 2，包含的表达式不能超过一个；
@@ -19,7 +19,7 @@
 # 空
 # ....
 
-# 参数表达式如：
+# 表达式如：
 # 1
 # None
 # a+b
@@ -27,6 +27,20 @@
 # 1 if a >10 else 0
 # ......
 # ********************************************************************************************************************
+
+
+lambda x:y: x+y
+
+
+def test(x,y):
+    ee
+    exit(e
+         e
+    e
+    exit())
+    return x+y
+
+
 
 # 1，标准匿名函数格式
 p = lambda x, y: x+y
@@ -38,18 +52,22 @@ print(a)  # <function <lambda> at 0x000001F75FE0E160>
 print(type(a))  # <class 'function'>  lambda它是函数
 
 
-# 2，非标准你们函数写法，直接在匿名函数后接实参
+# 2，非标准函数写法，直接在匿名函数后接实参
 print((lambda x: x**2)(3))  # 9
 
 
 
 # 3，将lambda函数作为参数传递给其他函数，如filter、map、sorted、reduce等Python内置函数使用
 # filter() 过滤序列，过滤掉不符合条件的元素，返回一个迭代器对象，如果要转换为列表，可以使用 list() 来转换。
+# 该函数接收两个参数，第一个为函数，第二个为序列，序列的每个元素作为参数传递给函数进行判，然后返回 True 或 False，最后将返回 True 的元素放到新列表中。
 x = filter(lambda x: x % 3 == 0, [1, 2, 3, 4, 5, 6])
 print(list(x))  # [3, 6]
+print(tuple(x))
+print(set(x))
+
 
 # https://www.cnblogs.com/lincappu/p/8179475.html
-# map() 会根据提供的函数对指定序列做映射。
+# map() 对函数做序列做映射
 # 当seq只有一个时，将函数func作用于这个seq的每个元素上，并得到一个新的seq
 squares = map(lambda x: x ** 2, range(5))
 print(list(squares))  # [0,1,4,9,16]
@@ -67,6 +85,7 @@ from functools import reduce
 print(reduce(lambda a,b:'{},{}'.format(a,b),[1,2,3,4,5,"sasa",7,8,9]))   # 1,2,3,4,5,sasa,7,8,9
 print(reduce(lambda a, b: a + b, [1, 2, 3, 4, 5, 6, 7, 8, 9]))  # 45
 print(reduce(lambda x, y: x * 10 + y, [1, 2, 3, 4, 5]))  # 12345
+
 
 
 # reduce() 按性别分组
