7f30fa9c3ead9e59a63d0eb21512e060d11940d6
jinhao <h.jin@zy-healthtech.com>
2021-10-18 18:00:18 +0800

    DESKTOP-DO7AEPU_20211018_8:0018

--- instance/zyjk/EHR/config/config.py
+++ instance/zyjk/EHR/config/config.py
@@ -20,13 +20,13 @@ varURL = "http://192.168.0.243:8082/"
 # dimPassword = "1qaz@WSX3edc"
 #
 
-# 初始化参数化
-varExcel = os.path.abspath(r"case.xls")
-varTimeYMDHSM = datetime.datetime.now().strftime('%Y%m%d%H%M%S')  # 获取当天日期时间,格式：20161020130318
-bk = xlrd.open_workbook(varExcel, formatting_info=True)
-newbk = copy(bk)
-sheetMain = bk.sheet_by_name("main")
-sheetTestCase = bk.sheet_by_name("testcase")
-styleRed = xlwt.easyxf('font: name Times New Roman, color-index red')
-styleBlue = xlwt.easyxf('font: name Times New Roman, color-index blue')
-styleGray25 = xlwt.easyxf('font: name Times New Roman, color-index gray25')
+# # 初始化参数化
+# varExcel = os.path.abspath(r"case.xls")
+# varTimeYMDHSM = datetime.datetime.now().strftime('%Y%m%d%H%M%S')  # 获取当天日期时间,格式：20161020130318
+# bk = xlrd.open_workbook(varExcel, formatting_info=True)
+# newbk = copy(bk)
+# sheetMain = bk.sheet_by_name("main")
+# sheetTestCase = bk.sheet_by_name("testcase")
+# styleRed = xlwt.easyxf('font: name Times New Roman, color-index red')
+# styleBlue = xlwt.easyxf('font: name Times New Roman, color-index blue')
+# styleGray25 = xlwt.easyxf('font: name Times New Roman, color-index gray25')
