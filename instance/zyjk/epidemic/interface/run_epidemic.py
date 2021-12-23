aca62b56545b2d6e97df6e90c39c613b84dd9ce6
jinhao <h.jin@zy-healthtech.com>
2021-12-23 18:17:28 +0800

    DESKTOP-DO7AEPU_20211223_8:1727

--- instance/zyjk/epidemic/interface/run_epidemic.py
+++ instance/zyjk/epidemic/interface/run_epidemic.py
@@ -3,8 +3,8 @@
 # Author        : John
 # Date          : 2021-11-15
 # Description   : 招远防疫 epidemic 接口自动化
-# 接口文档1：http://192.168.0.237:8001/swagger-ui.html
-# 接口文旦2：http://192.168.0.243:8001/doc.html#/
+# swagger接口文档：http://192.168.0.237:8001/swagger-ui.html
+# hfa系统用户中心api文档：http://192.168.0.243:8001/doc.html#/
 # web页：http://192.168.0.243:8002/admin/notice/index  （测试243， 开发237）
 # pip3 install nose-parameterized   for cmd
 # pip3 install BeautifulReport for cmd
