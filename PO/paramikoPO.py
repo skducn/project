1251912fc654cbc1bcccaf306e3ff2badde84b19
jinhao <h.jin@zy-healthtech.com>
2022-06-06 18:02:37 +0800

    ok2

--- PO/paramikoPO.py
+++ PO/paramikoPO.py
@@ -7,20 +7,20 @@
 # pip install paramiko
 # paramiko有两个模块SSHClient()和SFTPClient()
 # 参考：https://www.cnblogs.com/qianyuliang/p/6433250.html
+# https://blog.csdn.net/zangba9624/article/details/118398648
 #***************************************************************
+
+
 import paramiko
-import uuid
 
 class SSHConnection(object):
 
-    def __init__(self, host='192.168.0.243 ', port=22, username='root', pwd='Benetech79$#-'):
+    def __init__(self, host, port, username, pwd):
         self.host = host
         self.port = port
         self.username = username
         self.pwd = pwd
         self.__k = None
-
-    def connect(self):
         transport = paramiko.Transport((self.host, self.port))
         transport.connect(username=self.username,password=self.pwd)
         self.__transport = transport
@@ -28,31 +28,42 @@ class SSHConnection(object):
     def close(self):
         self.__transport.close()
 
-    def upload(self,local_path,target_path):
-        # 连接，上传
-        # file_name = self.create_file()
+    def upload(self, local_path, target_path):
+        # 上传
         sftp = paramiko.SFTPClient.from_transport(self.__transport)
-        # 将location.py 上传至服务器 /tmp/test.py
         sftp.put(local_path, target_path)
 
-    def download(self,remote_path,local_path):
+    def download(self, remote_path, local_path):
+        # 下载
         sftp = paramiko.SFTPClient.from_transport(self.__transport)
-        sftp.get(remote_path,local_path)
+        sftp.get(remote_path, local_path)
 
     def cmd(self, command):
+        # 执行服务器命令
         ssh = paramiko.SSHClient()
         ssh._transport = self.__transport
-        # 执行命令
         stdin, stdout, stderr = ssh.exec_command(command)
-        # 获取命令结果
         result = stdout.read()
-        print (str(result, encoding='utf-8'))
+        print(str(result, encoding='utf-8'))
         return result
 
-ssh = SSHConnection()
-ssh.connect()
-# ssh.upload('c:\startIIS.bat','/Users/linghuchong/Downloads/startIIS.bat')   # 上传（本地文件，远端文件）
-# ssh.download('/Users/linghuchong/Downloads/test.jpeg', 'kkkk.jpeg')   # 下载（远端文件，本地文件）
-# ssh.cmd("ls Downloads/")  # 显示远程文档内容
-ssh.cmd("dir")  # 显示远程文档内容
-ssh.close()
+
+
+if __name__ == "__main__":
+
+    ssh = SSHConnection(host='192.168.0.243', port=22, username='root', pwd='Benetech79$#-')
+
+    print("1 上传（本地文件，远端文件）".center(100, "-"))
+    ssh.upload('d:\\111.txt', '/root/111.txt')   # 上传（本地文件，远端文件）
+
+
+    print("2 下载（远端文件，本地文件）".center(100, "-"))
+    ssh.download('/root/111.txt', 'd:\\222.txt')   # 下载（远端文件，本地文件）
+
+
+    print("3 执行服务器命令".center(100, "-"))
+    ssh.cmd("cat 111.txt")  # 查看服务器上文件内容
+    ssh.cmd("ls")
+
+
+    ssh.close()
