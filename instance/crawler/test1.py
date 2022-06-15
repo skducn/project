e850ca2241e4863597d4e7b6a1edabcd1bb5d760
skducn <skducn@163.com>
2022-06-15 16:34:09 +0800

    fromMac

--- instance/crawler/test1.py
+++ instance/crawler/test1.py
@@ -16,16 +16,29 @@
 
 
 
-import requests
+# import requests
+#
+# url = "https://m.mallcoo.cn/api/gift/giftmanager/GetGiftInfoList"
+# header = {
+#     "content-type": "application/json",
+#     "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 15_0_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.20(0x18001441) NetType/WIFI Language/zh_CN",
+# }
+# data = "{\"mid\":10540,\"pi\":1,\"ps\":4,\"cid\":0,\"bs\":\"\",\"Header\":{\"Token\":\"1w10x6GX5Eis35Hbd8i7AwmqBsLHILqE,15577\",\"systemInfo\":{\"model\":\"iPhone 8 Plus (GSM+CDMA)<iPhone10,2>\",\"SDKVersion\":\"2.24.3\",\"system\":\"iOS 15.0.2\",\"version\":\"8.0.20\",\"miniVersion\":\"DZ.2.5.50.2SBGC.49.M\"}}}"
+# r = requests.post(url=url,data=data,headers=header)
+# print(r.text)
+# "content-type": "text/html; charset=gbk",
 
-url = "https://m.mallcoo.cn/api/gift/giftmanager/GetGiftInfoList"
-header = {
-    "content-type": "application/json",
-    "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 15_0_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.20(0x18001441) NetType/WIFI Language/zh_CN",
-}
-data = "{\"mid\":10540,\"pi\":1,\"ps\":4,\"cid\":0,\"bs\":\"\",\"Header\":{\"Token\":\"1w10x6GX5Eis35Hbd8i7AwmqBsLHILqE,15577\",\"systemInfo\":{\"model\":\"iPhone 8 Plus (GSM+CDMA)<iPhone10,2>\",\"SDKVersion\":\"2.24.3\",\"system\":\"iOS 15.0.2\",\"version\":\"8.0.20\",\"miniVersion\":\"DZ.2.5.50.2SBGC.49.M\"}}}"
-r = requests.post(url=url,data=data,headers=header)
-print(r.text)
+import requests
+url = "http://192.168.0.65/logincheck.php"
+header = {"content-type": "application/x-www-form-urlencoded"}
+d_iParam = {'USERNAME':"niuxuebin"}
+r = requests.post(url, headers=header, data=d_iParam, verify=False)
+a = r.cookies.get_dict()
+url = "http://192.168.0.65/general/appbuilder/web/business/product/crm"
+r = requests.get(url, headers={"Cookie":"PHPSESSID=" + a["PHPSESSID"]}, verify=False)
+print(r.url)
+
+# print(str(t_record[j]).encode('gbk', 'ignore').decode('gbk'))
 
 # curl -H "Host: m.mallcoo.cn" -H "content-type: application/json" -H "User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 15_0_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.20(0x18001441) NetType/WIFI Language/zh_CN" -H
 #
@@ -35,4 +48,4 @@ print(r.text)
 
 
 
-{"c":13,"m":1,"d":[{"id":184234,"n":"洛斐机械键盘套装","p":"sp_mall/71/5a/lf/0a-98fb-4f1c-9ba7-fc0a8a3a7ca5.jpg","b":40000.0,"sc":3,"ec":0,"rc":0,"mcc":1,"iht":true,"tn":"sp_mall/76/5d/ld/94-077b-4351-9c86-1369a46a3e50.jpg","OrderRank":0,"GiftCategoryID":4485,"CurrencyType":1,"CurrencyValue":0.0,"OnlineTime":"2022-05-25 00:00:00","OfflineTime":"2022-06-30 23:55:00","IsNeedCard":false,"MPPrice":0.0,"IsMPGiftUseNow":false,"IsAdvancedRestriction":false,"AdvancedRestriction":1,"MallCardTypeList":[],"MarketPrice":null,"PayType":0,"PayMoney":0.0,"CouponType":null,"IsNotShowSellStock":false,"ExchangeStartTime":"2022-05-25 00:00:00","ExchangeEndTime":"2022-06-30 23:55:00","IsOpenDiscount":true,"DiscountBonus":35000.0,"DiscountPayMoney":0.0,"IsVipPrice":false,"CurrentTime":"2022-05-25 22:32:36","IsAboutBegin":false,"Remark":""},{"id":183787,"n":"儿童口罩单片装","p":"sp_mall/75/57/h9/56-e9fc-4eda-8daf-f40c47b97479.jpg","b":50.0,"sc":200,"ec":0,"rc":0,"mcc":1,"iht":true,"tn":"sp_mall/72/55/he/a9-28cf-4fb8-acae-73cf86521ad3.jpg","OrderRank":0,"GiftCategoryID":4485,"CurrencyType":1,"CurrencyValue":0.0,"OnlineTime":"2022-05-25 00:00:00","OfflineTime":"2022-06-30 23:55:00","IsNeedCard":false,"MPPrice":0.0,"IsMPGiftUseNow":false,"IsAdvancedRestriction":false,"AdvancedRestriction":1,"MallCardTypeList":[],"MarketPrice":null,"PayType":0,"PayMoney":0.0,"CouponType":null,"IsNotShowSellStock":false,"ExchangeStartTime":"2022-05-25 00:00:00","ExchangeEndTime":"2022-06-30 23:55:00","IsOpenDiscount":true,"DiscountBonus":40.0,"DiscountPayMoney":0.0,"IsVipPrice":false,"CurrentTime":"2022-05-25 22:32:36","IsAboutBegin":false,"Remark":""},{"id":183783,"n":"冰雪奇缘24色彩铅","p":"sp_mall/73/58/h6/63-6ea5-4b62-89a2-33423359c971.jpg","b":500.0,"sc":20,"ec":0,"rc":0,"mcc":1,"iht":true,"tn":"sp_mall/7a/5b/h3/26-f763-4378-9bb5-809b9fbedfcb.jpg","OrderRank":0,"GiftCategoryID":4485,"CurrencyType":1,"CurrencyValue":0.0,"OnlineTime":"2022-05-25 00:00:00","OfflineTime":"2022-06-30 23:55:00","IsNeedCard":false,"MPPrice":0.0,"IsMPGiftUseNow":false,"IsAdvancedRestriction":false,"AdvancedRestriction":1,"MallCardTypeList":[],"MarketPrice":null,"PayType":0,"PayMoney":0.0,"CouponType":null,"IsNotShowSellStock":false,"ExchangeStartTime":"2022-05-25 00:00:00","ExchangeEndTime":"2022-06-30 23:55:00","IsOpenDiscount":true,"DiscountBonus":400.0,"DiscountPayMoney":0.0,"IsVipPrice":false,"CurrentTime":"2022-05-25 22:32:36","IsAboutBegin":false,"Remark":""},{"id":183782,"n":"端午舞龙手工艺品","p":"sp_mall/7f/58/hb/3b-c61a-4a23-ac51-3866ea31c9d8.jpg","b":900.0,"sc":50,"ec":0,"rc":0,"mcc":1,"iht":true,"tn":"sp_mall/7a/52/h2/6f-5341-4abf-9ff5-3cde1a4661c4.jpg","OrderRank":0,"GiftCategoryID":4485,"CurrencyType":1,"CurrencyValue":0.0,"OnlineTime":"2022-05-25 00:00:00","OfflineTime":"2022-06-30 23:55:00","IsNeedCard":false,"MPPrice":0.0,"IsMPGiftUseNow":false,"IsAdvancedRestriction":false,"AdvancedRestriction":1,"MallCardTypeList":[],"MarketPrice":null,"PayType":0,"PayMoney":0.0,"CouponType":null,"IsNotShowSellStock":false,"ExchangeStartTime":"2022-05-25 00:00:00","ExchangeEndTime":"2022-06-30 23:55:00","IsOpenDiscount":true,"DiscountBonus":800.0,"DiscountPayMoney":0.0,"IsVipPrice":false,"CurrentTime":"2022-05-25 22:32:36","IsAboutBegin":false,"Remark":""}],"e":null}
+# {"c":13,"m":1,"d":[{"id":184234,"n":"洛斐机械键盘套装","p":"sp_mall/71/5a/lf/0a-98fb-4f1c-9ba7-fc0a8a3a7ca5.jpg","b":40000.0,"sc":3,"ec":0,"rc":0,"mcc":1,"iht":true,"tn":"sp_mall/76/5d/ld/94-077b-4351-9c86-1369a46a3e50.jpg","OrderRank":0,"GiftCategoryID":4485,"CurrencyType":1,"CurrencyValue":0.0,"OnlineTime":"2022-05-25 00:00:00","OfflineTime":"2022-06-30 23:55:00","IsNeedCard":false,"MPPrice":0.0,"IsMPGiftUseNow":false,"IsAdvancedRestriction":false,"AdvancedRestriction":1,"MallCardTypeList":[],"MarketPrice":null,"PayType":0,"PayMoney":0.0,"CouponType":null,"IsNotShowSellStock":false,"ExchangeStartTime":"2022-05-25 00:00:00","ExchangeEndTime":"2022-06-30 23:55:00","IsOpenDiscount":true,"DiscountBonus":35000.0,"DiscountPayMoney":0.0,"IsVipPrice":false,"CurrentTime":"2022-05-25 22:32:36","IsAboutBegin":false,"Remark":""},{"id":183787,"n":"儿童口罩单片装","p":"sp_mall/75/57/h9/56-e9fc-4eda-8daf-f40c47b97479.jpg","b":50.0,"sc":200,"ec":0,"rc":0,"mcc":1,"iht":true,"tn":"sp_mall/72/55/he/a9-28cf-4fb8-acae-73cf86521ad3.jpg","OrderRank":0,"GiftCategoryID":4485,"CurrencyType":1,"CurrencyValue":0.0,"OnlineTime":"2022-05-25 00:00:00","OfflineTime":"2022-06-30 23:55:00","IsNeedCard":false,"MPPrice":0.0,"IsMPGiftUseNow":false,"IsAdvancedRestriction":false,"AdvancedRestriction":1,"MallCardTypeList":[],"MarketPrice":null,"PayType":0,"PayMoney":0.0,"CouponType":null,"IsNotShowSellStock":false,"ExchangeStartTime":"2022-05-25 00:00:00","ExchangeEndTime":"2022-06-30 23:55:00","IsOpenDiscount":true,"DiscountBonus":40.0,"DiscountPayMoney":0.0,"IsVipPrice":false,"CurrentTime":"2022-05-25 22:32:36","IsAboutBegin":false,"Remark":""},{"id":183783,"n":"冰雪奇缘24色彩铅","p":"sp_mall/73/58/h6/63-6ea5-4b62-89a2-33423359c971.jpg","b":500.0,"sc":20,"ec":0,"rc":0,"mcc":1,"iht":true,"tn":"sp_mall/7a/5b/h3/26-f763-4378-9bb5-809b9fbedfcb.jpg","OrderRank":0,"GiftCategoryID":4485,"CurrencyType":1,"CurrencyValue":0.0,"OnlineTime":"2022-05-25 00:00:00","OfflineTime":"2022-06-30 23:55:00","IsNeedCard":false,"MPPrice":0.0,"IsMPGiftUseNow":false,"IsAdvancedRestriction":false,"AdvancedRestriction":1,"MallCardTypeList":[],"MarketPrice":null,"PayType":0,"PayMoney":0.0,"CouponType":null,"IsNotShowSellStock":false,"ExchangeStartTime":"2022-05-25 00:00:00","ExchangeEndTime":"2022-06-30 23:55:00","IsOpenDiscount":true,"DiscountBonus":400.0,"DiscountPayMoney":0.0,"IsVipPrice":false,"CurrentTime":"2022-05-25 22:32:36","IsAboutBegin":false,"Remark":""},{"id":183782,"n":"端午舞龙手工艺品","p":"sp_mall/7f/58/hb/3b-c61a-4a23-ac51-3866ea31c9d8.jpg","b":900.0,"sc":50,"ec":0,"rc":0,"mcc":1,"iht":true,"tn":"sp_mall/7a/52/h2/6f-5341-4abf-9ff5-3cde1a4661c4.jpg","OrderRank":0,"GiftCategoryID":4485,"CurrencyType":1,"CurrencyValue":0.0,"OnlineTime":"2022-05-25 00:00:00","OfflineTime":"2022-06-30 23:55:00","IsNeedCard":false,"MPPrice":0.0,"IsMPGiftUseNow":false,"IsAdvancedRestriction":false,"AdvancedRestriction":1,"MallCardTypeList":[],"MarketPrice":null,"PayType":0,"PayMoney":0.0,"CouponType":null,"IsNotShowSellStock":false,"ExchangeStartTime":"2022-05-25 00:00:00","ExchangeEndTime":"2022-06-30 23:55:00","IsOpenDiscount":true,"DiscountBonus":800.0,"DiscountPayMoney":0.0,"IsVipPrice":false,"CurrentTime":"2022-05-25 22:32:36","IsAboutBegin":false,"Remark":""}],"e":null}
