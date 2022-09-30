eae123b08ed0a9c6bf6c9f27871f902e784af7ae
jinhao <h.jin@zy-healthtech.com>
2022-09-30 09:25:57 +0800

    111

--- PO/RePO.py
+++ PO/RePO.py
@@ -81,5 +81,7 @@ if __name__ == "__main__":
     print(Re_PO.subBlank("JGood is a handsome boy, he is cool, clever, and so on...", ""))   # 去掉字符串中的空格 JGoodisahandsomeboy,heiscool,clever,andsoon...
     print(Re_PO.split_list("JGood is a handsome boy, he is cool, clever, and so on...", r'\W+', 0)) # ['JGood', 'is', 'a', 'handsome', 'boy', 'he', 'is', 'cool', 'clever', 'and', 'so', 'on', '']
     print(Re_PO.split_list("JGood is a handsome boy, he is cool, clever, and so on...", r'(\W+)', 0))
+
     x = (Re_PO.split_list("0etrerta3B9", '[a-z]+', 0))  # ['0', '3', '9']
+
     print(Re_PO.findall_list("JGood is a handsome boy, he is cool, clever, and so on...", r'\w*oo\w*'))  # 匹配所有包含'oo'的单词 ， ['JGood', 'cool']
