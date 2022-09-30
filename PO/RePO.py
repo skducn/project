d3a49d2966e7a489ffb21127afdb25624bad4ce3
jinhao <h.jin@zy-healthtech.com>
2022-09-30 16:01:37 +0800

    111

--- PO/RePO.py
+++ PO/RePO.py
@@ -10,6 +10,17 @@
 # raw string 就是用'r'作为字符串的前缀，如 r"\n"：表示两个字符"\"和"n"，而不是换行符了，Python中写正则表达式时推荐使用这种形式。
 # 注意：在操作写文件路径时，切记不能使用 raw string ，这里存在陷阱。
 '''
+
+re.sub() 把字符串中所有匹配正则表达式的地方替换成新的字符串。
+re.sub(pattern, repl, string, count=0, flags=0)
+# pattern：正则中的模式字符串；
+# repl：要替换的字符串（即匹配到pattern后替换为repl），也可以是个函数；
+# string：原始字符串；
+# count：可选参数，表示要替换的最大次数，而且必须是非负整数，该参数默认为0，即所有的匹配都会替换；
+# flags：可选参数，表示编译时用的匹配模式（如忽略大小写、多行模式等），数字形式，默认为0。
+# # 找到RE匹配的所有子串，并将其用一个不同的字符串替换, count用于指定最多替换次数，不指定时全部替换。
+
+
 re.match(),从字符串的起点开始做匹配，匹配成功，返回一个匹配的对象，否则返回None
 m = re.match(r'hello', 'hello world!')
 print(m.group())  # hello
@@ -18,11 +29,7 @@ re.search()，扫描整个字符串并返回第一个成功的匹配
 re.findall()，在字符串中找到正则表达式所匹配的所有子串，并返回一个列表，如果没有找到匹配的，则返回空列表。
 re.split()，将一个字符串按照正则表达式匹配结果进行分割，返回列表类型。
 re.finditer()，在字符串中找到正则表达式所匹配的所有子串，并把它么作为一个迭代器返回。
-re.sub()，把字符串中所有匹配正则表达式的地方替换成新的字符串。
-re.sub(pattern, repl, string[, count=0])
-# print(re.sub("\s+", "?", text, count=2)  #  hello?111?world 1132 , 只匹配前2个
 
-# # 找到RE匹配的所有子串，并将其用一个不同的字符串替换, count用于指定最多替换次数，不指定时全部替换。
 
 re.compile(strPattern[,flag]) , 这个是Pattern类的工厂方法，用于将字符串形式的正则表达式编译为Pattern对象。
 第二个参数flag是匹配模式，取值可以使用按位或运算符'|'表示同时生效，比如re.I | re.M。另外，你也可以在regex字符串中指定模式，比如re.compile('pattern', re.I | re.M)与re.compile('(?im)pattern')是等价的。
@@ -43,6 +50,7 @@ a = re.compile(r"""\d +  # the integral part
 b = re.compile(r"\d+\.\d*")
 '''
 
+
 import re
 
 class RePO():
@@ -50,25 +58,6 @@ class RePO():
     def __init__(self):
         pass
 
-    def subBlank(self, varText, toSub):
-        # 将 varText 中空格匹配成 varRe
-        return re.sub(r'\s', lambda m: '[' + m.group(0) + ']', varText, 0).replace("[ ]", toSub)
-        # print(regex.sub(lambda m: '[' + m.group(0) + ']', varText))  # 将字符串中含有'oo'的单词用[]括起来 ， [JGood] is a handsome boy, he is [cool], clever, and so on...
-
-    def split_list(self, varText, varRe, varMaxsplit):
-        # maxsplit是分离的次数，maxsplit = 1 ， 分离一次，默认为0，不限制次数。
-        return (re.split(varRe, varText, varMaxsplit, flags=re.IGNORECASE))
-
-    def findall_list(self, varText, varRe):
-        # 匹配 varRe，列表形式返回 匹配到的单词。
-        return re.findall(varRe, varText)
-
-    # def finditer(self, varText, varRe):
-    #     # re.finditer(pattern, string, flags=0)
-    #     # 匹配 varRe，并与 varParma 进行运算后返回一个列表。
-    #     it = re.finditer(varRe, varText)
-    #     for m in it:
-    #         print(m.group()
 
     def purge(self):
         # 清空缓存中的正则表达式
@@ -78,13 +67,37 @@ if __name__ == "__main__":
 
     Re_PO = RePO()
 
-    print(Re_PO.subBlank("JGood is a handsome boy, he is cool, clever, and so on...", ""))   # 去掉字符串中的空格 JGoodisahandsomeboy,heiscool,clever,andsoon...
-    print(Re_PO.split_list("JGood is a handsome boy, he is cool, clever, and so on...", r'\W+', 0)) # ['JGood', 'is', 'a', 'handsome', 'boy', 'he', 'is', 'cool', 'clever', 'and', 'so', 'on', '']
-    print(Re_PO.split_list("JGood is a handsome boy, he is cool, clever, and so on...", r'(\W+)', 0))
 
-    x = (Re_PO.split_list("0etrerta3B9", '[a-z]+', 0))  # ['0', '3', '9']
+    # 匹配所有包含'oo'的单词
+    print(re.findall(r'\w*oo\w*', "JGood is a handsome boy, he is cool, clever, and so on..."))  #  ['JGood', 'cool']
 
 
+    # 去掉空格
+    print(re.sub(r"\s", "", "hello 111 world"))  #  hello111world
+
+    # 数字替换成*
+    print(re.sub(r'[0-9]', "*", "44799217,,,###,,535646343@qq.com. Today is 2021/12/21"))  # ********,,,###,,*********@qq.com. Today is ****/**/**
 
 
-    print(Re_PO.findall_list("JGood is a handsome boy, he is cool, clever, and so on...", r'\w*oo\w*'))  # 匹配所有包含'oo'的单词 ， ['JGood', 'cool']
+    # 字符串分割成列表
+    print(re.split(r'\W+', "JGood is a ,  so on..+——.", 0, flags=re.IGNORECASE))  # ['JGood', 'is', 'a', 'so', 'on', '']
+    print(re.split(r'(\W+)', "JGood is a ,  so on..+——.", 0, flags=re.IGNORECASE))  # ['JGood', ' ', 'is', ' ', 'a', ' ,  ', 'so', ' ', 'on', '..+——.', '']
+    print(re.split(r'[a-zA-Z]?', "0etrerta3B9"))  # ['', '0', '', '', '', '', '', '', '', '3', '', '9', '']
+
+    randstr = "Here is Edureka as Edureka"
+    res = (re.search(r"Edureka", randstr))
+    print(res)   # <re.Match object; span=(8, 15), match='Edureka'>
+    print(res.group())  # Edureka
+    print(res.start())  # 8
+    print(res.end())  # 15
+    print(res.span())  # (8, 15)
+    print(res.string)  # "Here is Edureka"
+
+    pat = re.compile("[0-9]")
+    res = pat.search("abcd23ef4gh")
+    print(res)  # <re.Match object; span=(4, 5), match='2'>
+
+    str = 'response body:{"name":"张三","age":20,"Idcard_no":"4100058971","phone":"15011100001"}'
+    # 开始匹配
+    res = re.search("{.*?}", str)  # 万能的.*?
+    print(res.group())  # {"name":"张三","age":20,"Idcard_no":"4100058971","phone":"15011100001"}
