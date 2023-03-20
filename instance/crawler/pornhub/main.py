03ebb8b08a9f3b9352467b451b2b699e82d758e5
skducn <skducn@163.com>
2023-03-20 15:59:20 +0800

    fromMac

--- instance/crawler/pornhub/main.py
+++ instance/crawler/pornhub/main.py
@@ -10,11 +10,11 @@
 from PornhubPO import *
 Pornhub_PO = PornhubPO()
 
-# 1，初始化更新文件列表
-# Pornhub_PO.setFileList('000.txt')
+# 1，获取html页面中的视频链接，并保存到 2videoUrl
+# Pornhub_PO.html2url("html.txt", 'ph.txt', 'url.txt')
+# 2，初始化更新文件列表, 
+# Pornhub_PO.setFileList()
 
-# 2，获取html页面中的视频链接，并保存到 2videoUrl
-# Pornhub_PO.html2url("1html", '2phFolderUrl', '2folderUrl')
 
 # 3，下载
 # # 3.1.1 单个下载（存在则忽略）
@@ -24,20 +24,7 @@ Pornhub_PO = PornhubPO()
 # Pornhub_PO.downloadOneOver("/", "https://cn.pornhub.com/view_video.php?viewkey=ph633a3fbc372ee")  # 下载到当前 /Users/linghuchong/Downloads/eMule/pornhub
 
 # 3.2 批量下载
-Pornhub_PO.downloadMore('2folderUrl')
-
-
-
-
-
-
-
-
-
-
-
-# if platform.system() == "Windows":
-#     folder = Pornhub_PO.run("yinyleon", "d:/11/44")
+Pornhub_PO.downloadMore('url.txt')
 
 
 
