9357968e38fb88805db71590ed7278527e2d0f69
skducn <skducn@163.com>
2023-04-17 08:52:39 +0800

    fromMac

--- instance/crawler/pornhub/main.py
+++ instance/crawler/pornhub/main.py
@@ -13,10 +13,10 @@ Pornhub_PO = PornhubPO()
 
 
 # 2，获取html页面中的视频链接，并保存到 2videoUrl
-# Pornhub_PO.html2url("html.txt", 'ph.txt', 'url.txt')
+Pornhub_PO.html2url("html.txt", 'ph.txt', 'url.txt')
 
 # 1，初始化更新文件列表, 
-# Pornhub_PO.setFileList()
+Pornhub_PO.setFileList()
 
 # 3，下载
 # # 3.1.1 单个下载（存在则忽略）
@@ -26,7 +26,7 @@ Pornhub_PO = PornhubPO()
 # Pornhub_PO.downloadOneOver("/", "https://cn.pornhub.com/view_video.php?viewkey=ph633a3fbc372ee")  # 下载到当前 /Users/linghuchong/Downloads/eMule/pornhub
 
 # 3.2 批量下载
-Pornhub_PO.downloadMore('url.txt')
+# Pornhub_PO.downloadMore('url.txt')
 
 
 
