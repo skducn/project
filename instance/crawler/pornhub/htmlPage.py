84984b1b0ba975fb6da59b10ef3814a08d50a680
skducn <skducn@163.com>
2023-07-01 23:30:48 +0800

    fromMac

--- instance/crawler/pornhub/htmlPage.py
+++ instance/crawler/pornhub/htmlPage.py
@@ -2,14 +2,12 @@
 #***************************************************************
 # Author     : John
 # Created on : 2021-12-27
-# Description: pornhub 获取html页面中的视频链接，并保存到 2videoUrl
+# Description: pornhub ，解析html.txt中的视频链接
 #***************************************************************
 
 from PornhubPO import *
 Pornhub_PO = PornhubPO()
 
 Pornhub_PO.html2url("html.txt", 'ph.txt', 'url.txt')
-
-Pornhub_PO.setFileList()
 
 os.system("open ph.txt")
