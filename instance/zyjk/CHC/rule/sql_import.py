b229fd1e3f0f4dc11cf3e35e110b70bca6e44327
skducn <skducn@163.com>
2023-11-09 16:42:36 +0800

    fromMac

--- instance/zyjk/CHC/rule/sql_import.py
+++ instance/zyjk/CHC/rule/sql_import.py
@@ -14,7 +14,7 @@ Sql_chcPO.insertTbl('健康评估', '健康评估')
 # Sql_chcPO.insertTbl('儿童健康干预', '儿童健康干预')
 # Sql_chcPO.insertTbl('已患和高风险疾病评估', '疾病评估')
 
-# Sql_chcPO.insertTbl('疾病身份证', '疾病身份证')
+Sql_chcPO.insertTbl('疾病身份证', '疾病身份证')
 # Sql_chcPO.insertTbl('测试规则', '测试规则')
 
 
