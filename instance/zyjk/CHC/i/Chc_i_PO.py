682f29c8c2a4ff22b18e1e7ac02fd6b513923c89
skducn <skducn@163.com>
2024-06-18 20:04:36 +0800

    fromMac

--- instance/zyjk/CHC/i/Chc_i_PO.py
+++ instance/zyjk/CHC/i/Chc_i_PO.py
@@ -19,7 +19,6 @@
 #   # captcha:
 #   #   enabled: false    //去掉验证码
 
-
 # *****************************************************************
 
 
@@ -27,20 +26,21 @@ import subprocess, json
 
 class Chc_i_PO():
 
-    def __init__(self):
+    def __init__(self, sm_account):
         # 登录
         # 获取用户token(lbl,Ww123456) '{"password": "Ww123456", "username": "lbl"}'
         # -d '4fa9de3518e897f29468be4e4e3956e53bae3cbdb8a.. 是用sm2对'{"password": "Ww123456", "username": "lbl"}'的加密，
         # 非加密写法 -d '{"password": "Ww123456", "username": "lbl"}'
 
+        # todo chc-auth, 登录模块
+        # 登录
         self.ipAddr = "http://192.168.0.202:22081"
-        command = "curl -X POST '" + self.ipAddr + "/auth/login' -d '4fa9de3518e897f29468be4e4e3956e53bae3cbdb8a64310f41ecbdf76843eb4c81acf3721bf8113cf4d3563c698ab74e060989daab802154ea144938bfcdc228608df8ec3548140f9fe745b6fc11bdf0d1c679116d56648d55e362fd3334b0a5f2a0995918b49e3f273c96dc3fd9f1a0b719dccd5910039783c6315bbf2156432003e34d8dada6e81c58d42a674a455ccdfeb41323b379fe1c06ab36462' " \
-                  "-H 'Request-Origion:SwaggerBootstrapUi' -H 'accept:*/*' -H 'Authorization:' -H 'Content-Type:application/json'"
+        command = "curl -X POST '" + self.ipAddr + "/auth/login' -d '" + sm_account + "' -H 'Request-Origion:SwaggerBootstrapUi' -H 'accept:*/*' -H 'Authorization:' -H 'Content-Type:application/json'"
         p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         out, err = p.communicate()
         str_r = bytes.decode(out)
         d_r = json.loads(str_r)
-        print(d_r)  # {'code': 200, 'msg': None, 'data': {'access_token': 'eyJhbGciOiJIUzUxMiJ9.eyJhZmZpbGlhdGVkX2lkIjoiIiwidXNlcl9pZCI6ODIsImNhdGVnb3J5X2NvZGUiOiIxIiwidXNlcl9rZXkiOiIwYzU3YmM3OC05OTNiLTQ1M2ItYjZkMC0yMmNlZTBhMWFkNzMiLCJ0aGlyZF9ubyI6IjEyMzEyMyIsImhvc3BpdGFsX2lkIjoiMDAwMDAwMSIsInVzZXJuYW1lIjoi5YiY5paM6b6ZIiwiaG9zcGl0YWxfbmFtZSI6IumdmeWuieeyvuelnueXhemZoiIsImFmZmlsaWF0ZWRfbmFtZSI6IiJ9.-xh2D7Obdensd3OcL_dqRaA7Qs4I0l0h--3ZYpYifgBZBP16Gzzq24W3IxS8c5ofcQTNyczRK2e3JipcCuyTqg', 'expires_in': 30}}
+        # print(d_r)  # {'code': 200, 'msg': None, 'data': {'access_token': 'eyJhbGciOiJIUzUxMiJ9.eyJhZmZpbGlhdGVkX2lkIjoiIiwidXNlcl9pZCI6ODIsImNhdGVnb3J5X2NvZGUiOiIxIiwidXNlcl9rZXkiOiIwYzU3YmM3OC05OTNiLTQ1M2ItYjZkMC0yMmNlZTBhMWFkNzMiLCJ0aGlyZF9ubyI6IjEyMzEyMyIsImhvc3BpdGFsX2lkIjoiMDAwMDAwMSIsInVzZXJuYW1lIjoi5YiY5paM6b6ZIiwiaG9zcGl0YWxfbmFtZSI6IumdmeWuieeyvuelnueXhemZoiIsImFmZmlsaWF0ZWRfbmFtZSI6IiJ9.-xh2D7Obdensd3OcL_dqRaA7Qs4I0l0h--3ZYpYifgBZBP16Gzzq24W3IxS8c5ofcQTNyczRK2e3JipcCuyTqg', 'expires_in': 30}}
         try:
             # {'code': 200, 'msg': None, 'data': {'access_token': 'eyJhbGciOiJIUzUxMiJ9.eyJhZmZpbGlhdGVkX2lkIjoiIiwidXNlcl9pZCI6ODIsImNhdGVnb3J5X2NvZGUiOiIxIiwidXNlcl9rZXkiOiIwYzU3YmM3OC05OTNiLTQ1M2ItYjZkMC0yMmNlZTBhMWFkNzMiLCJ0aGlyZF9ubyI6IjEyMzEyMyIsImhvc3BpdGFsX2lkIjoiMDAwMDAwMSIsInVzZXJuYW1lIjoi5YiY5paM6b6ZIiwiaG9zcGl0YWxfbmFtZSI6IumdmeWuieeyvuelnueXhemZoiIsImFmZmlsaWF0ZWRfbmFtZSI6IiJ9.-xh2D7Obdensd3OcL_dqRaA7Qs4I0l0h--3ZYpYifgBZBP16Gzzq24W3IxS8c5ofcQTNyczRK2e3JipcCuyTqg', 'expires_in': 30}}
             self.token = d_r['data']['access_token']
@@ -49,8 +49,8 @@ class Chc_i_PO():
             self.token = d_r['code']
 
 
-    def curl(self, status, curl, token=""):
-        command = "curl -X " + status + " " + self.ipAddr + curl + " -H 'Request-Origion:SwaggerBootstrapUi' -H 'accept:*/*' -H 'Authorization:' -H 'Content-Type:application/json' -H 'Authorization:Bearer " + token + "'  -H 'token:" + token + "' "
+    def curl(self, varRequestMethod, varUrl):
+        command = "curl -X " + varRequestMethod + " " + self.ipAddr + varUrl + " -H 'Request-Origion:SwaggerBootstrapUi' -H 'accept:*/*' -H 'Authorization:' -H 'Content-Type:application/json' -H 'Authorization:Bearer " + self.token + "'  -H 'token:" + self.token + "' "
         # print(command)
         p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         out, err = p.communicate()
@@ -59,32 +59,134 @@ class Chc_i_PO():
         return d_r
 
 
-    def logout(self):
-        # 登出（登录模块）
-        return self.curl("DELETE", '/auth/logout', self.token)
 
-    def systemMenuInfoBySystemId(self):
-        # 根据用户ID获取能够使用的系统（系统信息表）
-        return self.curl("GET", "/system/sysSystem/systemMenuInfoBySystemId?0=950c364d4694618ca13897b742ac7db1752f96c4a778dcb046847e4004d3b62f96e6a125ec604492a0915a47d3b6f6ef87df2f8ec7e718dd308e52f74135ed223adbfeac733f4cc9616f97146cc572d8e748ce23514798982364bd5171e5291ff8c3c34ac2aa8d2d8796e92a4f3d", self.token)
+    # todo REST-用户信息表
+
+    def getFamilyDoc(self):
+        # 获取家庭医生
+        return self.curl("GET", "/system/sysUser/getFamilyDoc")
+
+    def getAssistantList(self):
+        # 获取家医助手
+        return self.curl("GET", "/system/sysUser/getAssistantList")
+
+    def getHealthManagerList(self):
+        # 获取健康管理师
+        return self.curl("GET", "/system/sysUser/getHealthManagerList")
+
+    def getUser(self):
+        # 根据用户名获取用户信息（chc-system, REST-用户信息表）
+        return self.curl("GET", "/system/sysUser/getUser/lbl")
+
+    def getUserByOrg(self):
+        # 根据机构获取医生
+        return self.curl("GET", "/system/sysUser/getUserByOrg")
+
+    def getUserConfigByThird(self, orgCode, thridNO):
+        # 获取用户配置信息（chc-system, REST-用户信息表）
+        return self.curl("GET", "/system/sysUser/getUserConfigByThird")
+
+    def getUserInfoByThirdNo(self, thirdNO):
+        # 根据用户名获取用户信息（chc-system, REST-用户信息表）
+        return self.curl("GET", "/system/sysUser/getUserInfoByThirdNo")
+
+    def getUserInfoByThirdNoAndOrgCode(self, orgCode, thirdNO):
+        # 根据用户名和机构号获取用户信息（chc-system, REST-用户信息表）
+        return self.curl("GET", "/system/sysUser/getUserInfoByThirdNoAndOrgCode")
+
+    def getUserInfoThirdInfo(self, orgCode, thirdNO):
+        # 根据用户名获取用户信息（chc-system, REST-用户信息表）
+        return self.curl("GET", "/system/sysUser/getUserInfoThirdInfo")
 
     def selectUserInfo(self):
-        # 根据token获取用户信息（用户信息表）
-        return self.curl("GET", "/system/sysUser/selectUserInfo?0=61b9ee3ad031da7b01c6429d5ad3b21757ee9766d5b9e964a77ce621d29bbf7e296f482360155e6e01b29bc557eeedf702b643456ba5b39fe6febf284537a91f88468105d513684ae1abd790025a95df6590470dcc6c5a21c79a105cce1cdbdd5d45", self.token)
+        # 根据token获取用户信息（chc-system, REST-用户信息表）
+        return self.curl("GET", "/system/sysUser/selectUserInfo?0=61b9ee3ad031da7b01c6429d5ad3b21757ee9766d5b9e964a77ce621d29bbf7e296f482360155e6e01b29bc557eeedf702b643456ba5b39fe6febf284537a91f88468105d513684ae1abd790025a95df6590470dcc6c5a21c79a105cce1cdbdd5d45")
+
+    def sysUser(self, id):
+        # 单条查询
+        return self.curl("GET", "/system/sysUser/" + str(id))
+
+
+
+    # todo REST-系统信息表
+
+    def querySystemRole(self, userId):
+        # 获取所有系统的角色
+        return self.curl("GET", "/system/sysSystem/querySystemRole?" + str(userId))
+
+    def systemMenuInfoBySystemId(self):
+        # 根据系统Id获取所有菜单
+        return self.curl("GET", "/system/sysSystem/systemMenuInfoBySystemId?0=950c364d4694618ca13897b742ac7db1752f96c4a778dcb046847e4004d3b62f96e6a125ec604492a0915a47d3b6f6ef87df2f8ec7e718dd308e52f74135ed223adbfeac733f4cc9616f97146cc572d8e748ce23514798982364bd5171e5291ff8c3c34ac2aa8d2d8796e92a4f3d")
+
+    def systemMenuInfo(self, systemId):
+        # 获取系统菜单
+        return self.curl("GET", "/system/sysSystem/systemMenuInfo?" + str(systemId))
+
+    def systemMenuInfoBySystemId(self, systemId):
+        # 根据系统Id获取所有菜单
+        return self.curl("GET", "/system/sysSystem/systemMenuInfoBySystemId?" + str(systemId))
+
+    def sysSystem(self, Id):
+        # 单条查询
+        return self.curl("GET", "/system/sysSystem/?" + str(Id))
+
+
+
+    # todo chc-auth, 登录模块
+
+    def logined(self, userName):
+        # 确认用户是否已经登录
+        return self.curl("POST", '/auth/logined')
+
+    def logout(self):
+        # 登出
+        return self.curl("DELETE", '/auth/logout')
+
+    def refresh(self):
+        # 刷新
+        return self.curl("POST", '/auth/refresh')
+
+    def thirdLogin(self, orgCode, thirdNo):
+        # 第三方登录
+        return self.curl("POST", '/auth/thirdLogin')
 
 
 # *****************************************************************
 
 if __name__ == "__main__":
 
-    # 登录
-    chc_i_PO = Chc_i_PO()
 
-    # 根据用户ID获取能够使用的系统（系统信息表）
-    print(chc_i_PO.systemMenuInfoBySystemId())
+    # 登录(lbl,Ww123456)
+    chc_i_PO = Chc_i_PO('9580414215bd76bf8ddd310c894fdfb155f439b427a43fb3dbb13a142055e4b7236fd7498a6e8d2febc7a44688c45d68c11606a34632ce07aa94d037124c0c15c0a19ab3c9f35bab234dd5bc8a3b37d419786c17b2e26d46d0f378e3691f2823e48804aecfb23ebc8511fd66e9b927bb5344d97a9f6c9c001ba4e76865f4890a5c6f7c21810fdedf6bbe85625e6ca990e1fe1cef025760c3382326c993')
 
-    # 根据token获取用户信息（用户信息表）
-    print(chc_i_PO.selectUserInfo())
 
-    # 登出
-    print(chc_i_PO.logout())
+    # todo chc-system, REST-系统信息表
+    # print(chc_i_PO.querySystemRole(userId))  # 获取所有系统的角色
+    print(chc_i_PO.systemMenuInfoBySystemId())  # 根据用户ID获取能够使用的系统
+    # print(chc_i_PO.systemMenuInfo(systemId))  # 获取系统菜单
+    # print(chc_i_PO.systemMenuInfoBySystemId(systemId))  # 根据系统Id获取所有菜单
+    # print(chc_i_PO.sysSystem(Id))  # 单条查询
+
+
+    # todo chc-system, REST-用户信息表
+    # print(chc_i_PO.getFamilyDoc())  # 获取家庭医生
+    # print(chc_i_PO.getAssistantList())  # 获取家医助手
+    # print(chc_i_PO.getHealthManagerList())  # 获取健康管理师
+    # print(chc_i_PO.getUser())  # 根据用户名获取用户信息
+    # print(chc_i_PO.getUserByOrg())  # 根据机构获取医生
+    # print(chc_i_PO.getUserConfigByThird(orgCode,thirdNO))  # 获取用户配置信息
+    # print(chc_i_PO.getUserInfoByThirdNo(thirdNO))  # 根据用户名获取用户信息
+    # print(chc_i_PO.getUserInfoByThirdNoAndOrgCode(orgCode,thirdNO))  # 根据用户名和机构号获取用户信息
+    # print(chc_i_PO.getUserInfoThirdInfo(orgCode,thirdNO))  # 根据用户名获取用户信息
+    print(chc_i_PO.selectUserInfo())  # 根据token获取用户信息
+    # print(chc_i_PO.sysUser(id))  # 单条查询
+
+
+
+    # todo chc-auth, 登录模块
+    # print(chc_i_PO.logined())  # 确认用户是否已经登录
+    print(chc_i_PO.logout())  # 登出
+    # print(chc_i_PO.refresh())  # 刷新
+    # print(chc_i_PO.thirdLogin(orgCode,thridNo))  # 第三方登录
+
 
